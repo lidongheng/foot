3d70f39b195658406800244339c6a8272a1689a0
lidongheng <huoyi-20@163.com>
2022-07-04 23:43:29 +0800

    tijiao

--- config/league.js
+++ config/league.js
@@ -3,23 +3,23 @@ const { dateFormat } = require('../utils')
 // 模块二：分析比赛的基础信息
 // 日职联 START --------------------------------
 // 联赛代码
-const leagueCode = '22'
+const leagueCode = '15'
 // 赛季
 const season = '2022'
 // 轮次
-const round = 12
+const round = 20
 // 主队
-const host = '维京'
+const host = '金泉尚武'
 // 客队
-const away = '罗森博格'
+const away = '济州联队'
 // 联赛
-const league = 'Eliteserien'
+const league = 'KLeague'
 // 主队阵型
-const hostFormations = ['433']
+const hostFormations = ['4231']
 // 客队阵型
-const awayFormations = ['433']
+const awayFormations = ['343']
 // 亚盘拉力
-const yapan = host
+const yapan = away
 // 大小拉力
 const daxiao = '大'
 // 模块一：不常变动的数据
@@ -32,10 +32,10 @@ const qiutanHeaders = {
 // 本期暂时不抓取
 // 爬虫页面 943 313
 const start_urls = [
-  `${BASE_URL}matchResult/${season}/s${leagueCode}.js?version=${dateFormat(new Date().getTime(), 'YYYYMMDDHH')}`,
+  `${BASE_URL}matchResult/${season}/s${leagueCode}_313.js?version=${dateFormat(new Date().getTime(), 'YYYYMMDDHH')}`,
   `${BASE_URL}letGoal/${season}/l${leagueCode}.js?flesh=${Math.random()}`,
   `${BASE_URL}bigSmall/${season}/bs${leagueCode}.js?flesh=${Math.random()}`,
-  'http://zq.titan007.com/analysis/2142709cn.htm'
+  'http://zq.titan007.com/analysis/2141233cn.htm'
 ]
 // 比赛补充时间
 const matchTimeAddObj = {
